38c234688728c928fcba7025ca2347aa65ee6cef
Alan Russell <alan.russell@digirati.com>
2021-02-10 18:02:20 +0000

    Add Test page with login

--- identity/webapp/pages/_app.tsx
+++ identity/webapp/pages/_app.tsx
@@ -1,3 +1,13 @@
-import App from '@weco/common/views/pages/_app';
+import { UserProvider } from '@auth0/nextjs-auth0';
+import { AppProps } from 'next/app';
 import '../styles.scss';
+
+function App({ Component, pageProps }: AppProps): JSX.Element {
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
+}
+
 export default App;
